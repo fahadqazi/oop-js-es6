7acd8653f1787ebeb4aaa1b904b01d907b98f363
Fahad Qazi <fahadqazi@gmail.com>
2017-07-13 23:52:21 +0100

    getters and setters:

--- class-basics/src/app.js
+++ class-basics/src/app.js
@@ -1,20 +1,18 @@
 class Drone{
-    constructor(id, name){
-        this.id = id;
-        this.name = name;
+    constructor(id){
+        this._id = id
     }
 
-    static getCompany(){
-        console.log('Drones incorporated!')
+    get id(){
+        return this._id;
     }
-    
-    fly(){
-        console.log('Drone ' + this.id + ' is flying');
+
+    set id(value){
+        this._id = value;
     }
 }
 
-let drone1 = new Drone('a123', 'Flyer');
-let drone2 = new Drone('b456', 'Twirler');
-
-drone1.fly();
-Drone.getCompany();
+let drone1 = new Drone('123');
+console.log(drone1.id);
+drone1.id = '456';
+console.log(drone1.id);
