c85271826c6558b0f88ccbf23ed382b77646e677
Fahad Qazi <fahadqazi@gmail.com>
2017-07-13 23:42:00 +0100

    adding static methods, only class specific

--- class-basics/src/app.js
+++ class-basics/src/app.js
@@ -3,6 +3,10 @@ class Drone{
         this.id = id;
         this.name = name;
     }
+
+    static getCompany(){
+        console.log('Drones incorporated!')
+    }
     
     fly(){
         console.log('Drone ' + this.id + ' is flying');
@@ -13,3 +17,4 @@ let drone1 = new Drone('a123', 'Flyer');
 let drone2 = new Drone('b456', 'Twirler');
 
 drone1.fly();
+Drone.getCompany();
