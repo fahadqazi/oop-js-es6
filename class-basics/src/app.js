22b18f7c11d39d34f08d5b4c371de769613adcff
Fahad Qazi <fahadqazi@gmail.com>
2017-07-13 23:38:59 +0100

    adding static or class property

--- class-basics/src/app.js
+++ class-basics/src/app.js
@@ -5,7 +5,13 @@ class Drone{
     }
 }
 
-let drone = new Drone('123', 'fahad');
+//setting static or class properties
+//this property is only available on the class, not on instances
+Drone.maxHeight = 2000;
 
-console.log(drone.id)
-console.log(drone.name);
+let drone1 = new Drone('a123', 'Flyer');
+let drone2 = new Drone('b456', 'Twirler');
+
+console.log(drone1.id + ' ' + drone2.id);
+
+console.log(Drone.maxHeight);
