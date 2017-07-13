a09f3f250f9929b597ec15a6b7e02bf731fdafdd
Fahad Qazi <fahadqazi@gmail.com>
2017-07-13 23:34:22 +0100

    project setup

---
+++ class-basics/src/app.js
@@ -0,0 +1,11 @@
+class Drone{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+let drone = new Drone('123', 'fahad');
+
+console.log(drone.id)
+console.log(drone.name);
