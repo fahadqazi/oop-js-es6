51394aa2f21276a2c8bc75ab005c89572a65b507
Fahad Qazi <fahadqazi@gmail.com>
2017-07-16 00:45:44 +0100

    added button

--- data-service-class/src/app.js
+++ data-service-class/src/app.js
@@ -1,31 +1,37 @@
+import $ from 'jquery';
 import {Car} from './classes/car.js';
 import {Drone} from './classes/drone.js';
 import {fleet} from './fleet-data.js';
 import {FleetDataService} from './services/fleet-data-service.js';
+import {Button} from './ui/button.js';
 
-let dataService = new FleetDataService();
-dataService.loadData(fleet);
+let b = new Button('Click Me');
+b.appendToElement($('body'))
 
-console.log(dataService);
 
-for (let car of dataService.cars){
-    console.log(car.license); 
-}
+// let dataService = new FleetDataService();
+// dataService.loadData(fleet);
 
-// this works if fleet-data.js is modified
-for (let errors of dataService.errors){
-    console.log(errors.message);
-}
+// console.log(dataService);
 
-let car = dataService.getCarByLicense('AT9900');
-console.log('Found the car: ', car);
+// for (let car of dataService.cars){
+//     console.log(car.license); 
+// }
 
-let cars = dataService.getCarsSortedByLicense();
-for (let car of cars){
-    console.log(car.license);
-}
+// // this works if fleet-data.js is modified
+// for (let errors of dataService.errors){
+//     console.log(errors.message);
+// }
 
-let filteredCars = dataService.filterCarsByMake('U');
-for (let car of filteredCars){
-    console.log(car.make);
-}
+// let car = dataService.getCarByLicense('AT9900');
+// console.log('Found the car: ', car);
+
+// let cars = dataService.getCarsSortedByLicense();
+// for (let car of cars){
+//     console.log(car.license);
+// }
+
+// let filteredCars = dataService.filterCarsByMake('U');
+// for (let car of filteredCars){
+//     console.log(car.make);
+// }
