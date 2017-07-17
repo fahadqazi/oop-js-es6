2973b0ffbd050f6677ec44246d4c0ae04bd017fe
Fahad Qazi <fahadqazi@gmail.com>
2017-07-17 23:06:20 +0100

    google maps set up

--- data-service-class/src/app.js
+++ data-service-class/src/app.js
@@ -7,14 +7,24 @@ import {Button} from './ui/button.js';
 import {Image} from './ui/image.js';
 import {TitleBar} from './ui/title-bar.js';
 import {DataTable} from './ui/data-table.js';
+import {GoogleMap} from './ui/google-maps.js';
 
-let headers = "License Make Model Miles".split(' ');
 let dataService = new FleetDataService();
 dataService.loadData(fleet);
-let dt = new DataTable(headers, dataService.cars);
-dt.appendToElement($('body'));
 
+let centerOfMap = {lat: 40.783661, lng: -73.965883};
+let map = new GoogleMap(centerOfMap, dataService.cars);
 
+map.appendToElement($('body'));
+
+// Data Tables
+// let headers = "License Make Model Miles".split(' ');
+// let dataService = new FleetDataService();
+// dataService.loadData(fleet);
+// let dt = new DataTable(headers, dataService.cars);
+// dt.appendToElement($('body'));
+
+// TitleBar
 // let tb = new TitleBar('Our Application');
 // tb.addLink('Home', '');
 // tb.addLink('Cars', '');
