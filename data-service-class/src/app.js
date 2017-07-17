2e4f36e2bcda68af6cd57f808511c11482a96b39
Fahad Qazi <fahadqazi@gmail.com>
2017-07-17 21:52:18 +0100

    adding title bar

--- data-service-class/src/app.js
+++ data-service-class/src/app.js
@@ -5,12 +5,16 @@ import {fleet} from './fleet-data.js';
 import {FleetDataService} from './services/fleet-data-service.js';
 import {Button} from './ui/button.js';
 import {Image} from './ui/image.js';
+import {TitleBar} from './ui/title-bar.js';
 
-let b = new Button('Click Me');
-b.appendToElement($('body'))
+let tb = new TitleBar('Our Application');
+tb.appendToElement($('body'));
 
-let i = new Image('../images/drone.jpg');
-i.appendToElement($('body'));
+// let b = new Button('Click Me');
+// b.appendToElement($('body'))
+
+// let i = new Image('../images/drone.jpg');
+// i.appendToElement($('body'));
 
 
 // let dataService = new FleetDataService();
